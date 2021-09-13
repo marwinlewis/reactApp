3a3cd8275ebde871694faca50c0c9517eb41e940
Marvin <marwinlewis@gmail.com>
2021-09-13 19:34:29 +0530

    Gulp setup.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
-"use strict"
-
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
 
-gulp.task('message', function(done){
-    console.log("What!");
-    done();
-});
+function style(){
+    return gulp.src('./src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./src/assets/css'))
+        .pipe(browserSync.stream());
+}
+
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: './src'
+        }
+    });
+    gulp.watch('./src/scss/**/*.scss', style);
+    gulp.watch('./src/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+exports.default = gulp.series(style, watch);
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './assets/css/styles.css';
 import Header from './components/header';
 import Footer from './components/footer';
 
